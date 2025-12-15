598c62b01824575e02b08abbbd5e4528ad998643
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-14 22:17:01 -0500

    add printBoard function

--- main.cpp
+++ main.cpp
@@ -3,19 +3,13 @@
 using namespace std;
 
 void waitForEnter();
+void printBoard(const int board[9][9]);
 
 int main()
 {
   int board[9][9] = {};
 
-  for (int i = 0; i < 9; i++)
-  {
-    for (int j = 0; j < 9; j++)
-    {
-      cout << board[i][j] << ' ';
-    }
-    cout << '\n';
-  }
+  printBoard(board);
 
   waitForEnter();
   return 0;
@@ -26,4 +20,24 @@ void waitForEnter()
   cout << "\nPress Enter to exit...";
   string dummy;
   getline(cin, dummy);
+}
+void printBoard(const int board[9][9])
+{
+  for (int i = 0; i < 9; i++)
+  {
+    if (i % 3 == 0 && i != 0)
+    {
+      cout << "------+-------+------\n";
+    }
+
+    for (int j = 0; j < 9; j++)
+    {
+      if (j % 3 == 0 && j != 0)
+      {
+        cout << "| ";
+      }
+      cout << board[i][j] << ' ';
+    }
+    cout << '\n';
+  }
 }
